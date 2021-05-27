b2de4a4eaa16f3062b21bd11d82db48d9d048301
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-05-27 09:52:54 +0200

    feat: us#1 aut#1 Guardian: invalid imput car not parked inside GREEN

--- src/carParkEscape.js
+++ src/carParkEscape.js
@@ -1,5 +1,6 @@
 function carParkEscape(building) {
-  if (!Array.isArray(building) || building.length === 0) return 'ERROR';
+  if (!Array.isArray(building) || building.length === 0 || building.indexOf(2) === -1)
+    return 'ERROR';
   return undefined;
 }
 
