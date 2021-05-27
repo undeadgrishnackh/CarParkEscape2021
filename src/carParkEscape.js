96b4afc4b9b7c29321584b344a3ffe7a767454b0
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-05-27 09:53:59 +0200

    feat: us#1 aut#1 Guardian: invalid imput car not parked inside REFACTOR

--- src/carParkEscape.js
+++ src/carParkEscape.js
@@ -1,6 +1,9 @@
+function isInvalidParking(building) {
+  return !Array.isArray(building) || building.length === 0 || building.indexOf(2) === -1;
+}
+
 function carParkEscape(building) {
-  if (!Array.isArray(building) || building.length === 0 || building.indexOf(2) === -1)
-    return 'ERROR';
+  if (isInvalidParking(building)) return 'ERROR';
   return undefined;
 }
 
