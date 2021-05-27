a76494a8f1c15520524377f41aa60cc6ecf98e2c
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-05-27 10:05:08 +0200

    feat: us#1 aut#1 Find my car GREEN and REFACTOR

--- src/carParkEscape.js
+++ src/carParkEscape.js
@@ -1,10 +1,15 @@
-function isInvalidParking(building) {
-  return !Array.isArray(building) || building.length === 0 || building.indexOf(2) === -1;
-}
+const isntAnArray = (building) => !Array.isArray(building);
+const isEmpty = (building) => building.length === 0;
+const myCarIsntThere = (building) => building.indexOf(2) === -1;
+const isInvalidParking = (building) =>
+  isntAnArray(building) || isEmpty(building) || myCarIsntThere(building);
+
+const inWhciParkingSlotIsMyCar = (building) => building.indexOf(2) + 1;
 
 function carParkEscape(building) {
   if (isInvalidParking(building)) return 'ERROR';
-  return undefined;
+
+  return inWhciParkingSlotIsMyCar(building);
 }
 
 module.exports = carParkEscape;
