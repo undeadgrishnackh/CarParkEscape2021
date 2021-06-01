85d04e49e5adb633b8872b985202c3c9d880e10f
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-06-01 13:36:15 +0200

    refactor: converted the ugly for loop into a more readable array iteration over a conditional rule

--- src/carparkBABU.js
+++ src/carparkBABU.js
@@ -5,21 +5,15 @@ const EMPTY_SLOT = 0;
 const isMyCarParkedOutsideTheParking = (building) => !building.includes(CAR);
 const isTheParkingAnEmptyBuilding = (building) => building.length === 0;
 const isTheParkingAWrongArray = (building) => !Array.isArray(building);
-const isInTheBuildingSomeStrangeItem = (building) => {
-  // !!! TECH_DEBT: complexity must be reduced!
-  for (let parkingSlot = 0; parkingSlot < building.length; parkingSlot += 1) {
-    if (building[parkingSlot] !== CAR && building[parkingSlot] !== EMPTY_SLOT) {
-      return true;
-    }
-  }
-  return false;
-};
+const checkIfSomeSlotsContainWrongElements = (element) => element !== EMPTY_SLOT && element !== CAR;
+const isInTheBuildingSomeStrangeItem = (building) =>
+  building.some(checkIfSomeSlotsContainWrongElements);
 
 const checkForWrongInput = (building) =>
   isTheParkingAWrongArray(building) ||
   isTheParkingAnEmptyBuilding(building) ||
-  isMyCarParkedOutsideTheParking(building) ||
-  isInTheBuildingSomeStrangeItem(building);
+  isInTheBuildingSomeStrangeItem(building) ||
+  isMyCarParkedOutsideTheParking(building);
 
 const floorSize = (building) => building.length;
 const whereIsMyCar = (building) => building.indexOf(CAR);
